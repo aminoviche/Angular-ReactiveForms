004d413775f136054d4d2ef3489e648136e498fb
aminoviche <aminoviche@gmail.com>
2023-01-14 16:43:23 +0100

    cross field validation

--- demo-start-v12/src/app/customers/customer.component_reactive.ts
+++ demo-start-v12/src/app/customers/customer.component_reactive.ts
@@ -10,48 +10,51 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponentReactive implements OnInit {
-  customerForm : FormGroup;
+  customerForm: FormGroup;
 
-  constructor(private fb : FormBuilder) {
+  constructor(private fb: FormBuilder) {
 
 
-   }
+  }
 
   ngOnInit(): void {
     this.customerForm = this.fb.group({
-      firstName : ['', [Validators.required,Validators.minLength(3)]],
-      lastName : ['jalol', [Validators.required,Validators.minLength(3)]],
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['jalol', [Validators.required, Validators.minLength(3)]],
       //lastName : {value : 'jalol',  disabled :true},
-      email : ['', [Validators.required, Validators.email]],
-      phone : [''],
-      notification : ['', [Validators.required]],
-     // rating : ['', ratingRangeWithoutParam],
-      rating : ['', ratingRange(1,5)],
-      sendCatalog :'true',
+      emailGroup: this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        confirmEmail: ['', [Validators.required]]
+      }),
+      phone: [''],
+      notification: ['', [Validators.required]],
+      // rating : ['', ratingRangeWithoutParam],
+      rating: ['', ratingRange(1, 5)],
+      sendCatalog: 'true',
     })
   }
 
   save(): void {
     console.log(this.customerForm);
     console.log('Saved:' + JSON.stringify(this.customerForm.value));
-    
+
   }
 
-  populateData(){
+  populateData() {
     //setValue
     this.customerForm.patchValue({
-      firstName :'allal',
-      lastName : 'joha',
-      email : 'joha@gmail.com',
-      sendCatalog : 'false'
+      firstName: 'allal',
+      lastName: 'joha',
+      email: 'joha@gmail.com',
+      sendCatalog: 'false'
     })
   }
 
-  setNotification(notifyVia : String) : void{
-     const phoneControl = this.customerForm.get('phone');
-    if(notifyVia == 'TEXT'){
+  setNotification(notifyVia: String): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia == 'TEXT') {
       phoneControl?.setValidators(Validators.required);
-    }else{
+    } else {
       phoneControl?.clearValidators();
     }
 
@@ -63,20 +66,20 @@ export class CustomerComponentReactive implements OnInit {
 }
 
 // Validation function without param
-function ratingRangeWithoutParam(c : AbstractControl):{ [key :string]: boolean } | null {
-  if(c.value != null && isNaN(c.value) || c.value < 1 || c.value >5){
-    return { 'range': true};
+function ratingRangeWithoutParam(c: AbstractControl): { [key: string]: boolean } | null {
+  if (c.value != null && isNaN(c.value) || c.value < 1 || c.value > 5) {
+    return { 'range': true };
   }
   return null;
 }
 
 
 // Validation function with param
-function ratingRange(min : number,  max: number) :ValidatorFn {
-return (c : AbstractControl):{ [key :string]: boolean } | null => {
-  if(c.value != null && isNaN(c.value) || c.value < min || c.value >max){
-    return { 'range': true};
-  }
-  return null;
+function ratingRange(min: number, max: number): ValidatorFn {
+  return (c: AbstractControl): { [key: string]: boolean } | null => {
+    if (c.value != null && isNaN(c.value) || c.value < min || c.value > max) {
+      return { 'range': true };
+    }
+    return null;
   }
 }
